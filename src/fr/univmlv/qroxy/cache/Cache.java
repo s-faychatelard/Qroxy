97399489e75514d9da3ebbad63f3ae3b37ca58a6
joachim <joachim@joachim-portable>
2012-05-31 19:01:14 +0200

    configuration and configurationtype classes

--- src/fr/univmlv/qroxy/cache/Cache.java
+++ src/fr/univmlv/qroxy/cache/Cache.java
@@ -4,7 +4,6 @@ package fr.univmlv.qroxy.cache;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
